960c786f46768155461c42b4001c09fdf1daf4b0
KRITI KUMARI <113662334+kritiKu@users.noreply.github.com>
2025-02-14 17:02:02 +0530

    Add files via upload

---
+++ Patterns/starPattern2.java
@@ -0,0 +1,11 @@
+public class starPattern2 {
+    public static void main(String[] args){
+        for(int i=1; i<=5; i++){
+            for(int j=i; j<6; j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
