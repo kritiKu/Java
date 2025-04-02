cc650b60a65feb83f885a236b75457c9080890bc
KRITI KUMARI <113662334+kritiKu@users.noreply.github.com>
2025-04-02 19:41:45 +0530

    Delete testingHello.java

--- testingHello.java
+++
@@ -1,9 +0,0 @@
-public class testingHello {
-    public static void main(String[] args) {
-        Hello h1 = new Hello(1);
-        Hello h2 = new Hello(2);
-
-        h1.start();
-        h2.start();
-    }
-}
